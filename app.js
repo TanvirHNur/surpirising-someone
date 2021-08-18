c4ad430d91f5df7c3f6ac720c9bb3ebd3a72482e
TanvirHNur <tanvir.du.ier@gmail.com>
2021-08-18 19:18:15 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+document.getElementById('my-btn').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#614092';
+    document.getElementById('main-text').style.display = 'none';
+    document.getElementById('love-you').style.display = 'block'
+})
+
+
+document.getElementById('my-btn1').addEventListener('click', function(){
+    document.body.style.backgroundColor = '#0f51a3';
+    document.getElementById('main-text').style.display = 'none';
+    document.getElementById('love-you').style.display = 'block'
+})
